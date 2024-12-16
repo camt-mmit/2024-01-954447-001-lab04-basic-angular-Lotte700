d696f203f252ba5277117dfb9f67f36884e14bb3
Lotte700 <139039799+Lotte700@users.noreply.github.com>
2024-12-16 16:59:49 +0700

    final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,31 @@
 import { Routes } from '@angular/router';
 import { HelloWordComponent } from './hello-word/hello-word.component';
+import { FriendComponent } from './friend/friend.component';
+import { ProfileComponent } from './profile/profile.component';
+import { StatusComponent } from './status/status.component';
+
 
 export const routes: Routes = [
   {
-    path: 'hello-word',
+  path:'',
+  redirectTo:'hello-word',
+  pathMatch:'full',
+  },
+  {
+    path: 'friend',
+    component:FriendComponent,
+  },
+  {
+    path: 'status',
+    component:StatusComponent,
+  },
+  {
+    path: 'profile',
+    component:ProfileComponent,
+  },
+  {
+    path: 'hello-world',
     component:HelloWordComponent,
   }
+
 ];
---
+++ src/app/status/status.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status',
+  imports: [],
+  templateUrl: './status.component.html',
+  styleUrl: './status.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class StatusComponent {
+
+}
